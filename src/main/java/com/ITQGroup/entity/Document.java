48cae107173e602efe7c2c44c138507d697f1a31
Kuleb9lka <winkeylinks@gmail.com>
2026-02-18 22:32:25 +0300

    added validations for all entities

--- src/main/java/com/ITQGroup/entity/Document.java
+++ src/main/java/com/ITQGroup/entity/Document.java
@@ -1,5 +1,6 @@
 package com.ITQGroup.entity;
 
+import com.ITQGroup.annotation.ValidDateRange;
 import com.ITQGroup.enums.DocumentStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -12,10 +13,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.JdbcType;
+import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
 import java.time.LocalDateTime;
@@ -23,33 +31,45 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "documents")
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@ValidDateRange(startField = "createDate", endField = "updateDate")
 public class Document {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
-    @Column(name = "unique_number", nullable = false)
-    private UUID uniqueNUmber;
+    @Column(name = "unique_number", nullable = false, unique = true, updatable = false)
+    @UuidGenerator
+    @NotNull(message = "Unique number can't be null")
+    private UUID uniqueNumber;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(name = "name", nullable = false)
+    @NotBlank(message = "Name can't be null or blank")
+    @Size(min = 5, message = "Minimal document name length 5 symbols")
     private String name;
 
     @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING)
     @JdbcType(PostgreSQLEnumJdbcType.class)
+    @NotNull(message = "Status can't be null")
     private DocumentStatus documentStatus;
 
     @Column(name = "create_date", nullable = false)
+    @NotNull(message = "Create date can't be null")
+    @PastOrPresent(message = "Create date can't be in future")
     private LocalDateTime createDate;
 
     @Column(name = "update_date")
+    @PastOrPresent(message = "Update date can't be in future")
     private LocalDateTime updateDate;
 }
--- src/main/java/com/ITQGroup/entity/History.java
+++ src/main/java/com/ITQGroup/entity/History.java
@@ -12,9 +12,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.JdbcType;
 import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
@@ -22,13 +27,16 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "history")
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class History {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -36,17 +44,17 @@ public class History {
     private User user;
 
     @Column(name = "update_date", nullable = false)
+    @NotNull(message = "Update date can't be null")
+    @PastOrPresent(message = "Update date can't be in future")
     private LocalDateTime updateDate;
 
     @Column(name = "action", nullable = false)
     @Enumerated(EnumType.STRING)
     @JdbcType(PostgreSQLEnumJdbcType.class)
+    @NotNull(message = "Action can't be null")
     private Action action;
 
     @Column(name = "comment")
+    @Size(max = 1000, message = "Max comment size 1000 symbols")
     private String comment;
-
-
-
-
 }
