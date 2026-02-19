88d78f775afee83d7343b711d23a80ec290a7dc1
Kuleb9lka <winkeylinks@gmail.com>
2026-02-19 15:23:30 +0300

    added mappers

---
+++ src/main/java/com/ITQGroup/mapper/ApprovalRegistryMapper.java
@@ -0,0 +1,15 @@
+package com.ITQGroup.mapper;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper
+        (
+                componentModel = "spring",
+                injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+                unmappedTargetPolicy = ReportingPolicy.ERROR,
+                uses = {DocumentMapper.class, UserMapper.class}
+        )
+public interface ApprovalRegistryMapper {
+}
