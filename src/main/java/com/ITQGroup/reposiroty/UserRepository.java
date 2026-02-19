9e96d81826bcef65f2f4860ccc6d53a35fc52d4b
Kuleb9lka <winkeylinks@gmail.com>
2026-02-19 10:10:39 +0300

    added base repositories

---
+++ src/main/java/com/ITQGroup/reposiroty/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ITQGroup.reposiroty;
+
+import com.ITQGroup.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
