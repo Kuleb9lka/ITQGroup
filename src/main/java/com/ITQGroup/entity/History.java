f34ea1ab3833d905d0d74a18b8c78de5a29272ab
Kuleb9lka <winkeylinks@gmail.com>
2026-02-19 15:13:56 +0300

    added document id column to history table

--- src/main/java/com/ITQGroup/entity/History.java
+++ src/main/java/com/ITQGroup/entity/History.java
@@ -40,6 +40,10 @@ public class History {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "document_id", nullable = false)
+    private Document document;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
