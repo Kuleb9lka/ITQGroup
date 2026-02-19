3a44cd924d310511b6f86a20af35d1657bfc9a35
Kuleb9lka <winkeylinks@gmail.com>
2026-02-19 17:17:40 +0300

    deleted users classes

--- src/main/java/com/ITQGroup/exception/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.ITQGroup.exception;
-
-public class UserAlreadyExistException extends RuntimeException{
-    public UserAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/ITQGroup/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ITQGroup.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
